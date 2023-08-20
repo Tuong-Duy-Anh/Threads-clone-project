86ff84ced9bc179ee16fd6f37d647f19d85d1955
Tuong-Duy-Anh <tdanh.ump@gmail.com>
2023-08-20 23:09:38 +0700

    seting up user adding

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,7 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-let isConnected = false; // variable to check connection status to mongoose 
+let isConnected = false; // variable to check connection status to mongoose
 
 export const connectToDB = async () => {
-    
-}
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
+  if (isConnected) return console.log("Connected to MongoDB");
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
